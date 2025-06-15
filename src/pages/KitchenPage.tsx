ca95774c742ef56f494463a8b81685707b4260ac
KvS-08 <kvsolutionshn@gmail.com>
2025-06-15 09:22:01 -0600

    Updated KitchenPage.tsx

--- src/pages/KitchenPage.tsx
+++ src/pages/KitchenPage.tsx
@@ -154,7 +154,7 @@ export const KitchenPage: React.FC = () => {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {orders.map((order) => (
             <OrderCard 
               key={order.id}
