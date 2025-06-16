202227a9a517019b3a7815a544c657cecc855b24
KvS-08 <kvsolutionshn@gmail.com>
2025-06-15 19:27:09 -0600

    Updated InventoryPage.tsx

--- src/pages/InventoryPage.tsx
+++ src/pages/InventoryPage.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { ThemeToggle } from '../components/ui/ThemeToggle';
+import { useState } from 'react';
 import { format } from 'date-fns';
+import AddIngredientsModal from '../components/kitchen/AddIngredientsModal';
 import { es } from 'date-fns/locale';
 
 const InventoryPage: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="space-y-6 md:ml-32 pt-4 md:pt-0 md:-mt-10">
+      <AddIngredientsModal isOpen={isModalOpen} onClose={handleCloseModal} />
       <div className="flex justify-between items-center">
         <h1 className="text-xl md:text-3xl font-bold">
           {(() => {
@@ -29,7 +41,12 @@ const InventoryPage: React.FC = () => {
       </div>
       <div className="mb-4 flex justify-end items-center space-x-2">
         <input type="date" className="p-0 border border-gray-300 rounded-md bg-white text-black dark:bg-gray-700 dark:border-gray-600 dark:text-white text-center" defaultValue={new Date().toISOString().split('T')[0]} />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-1 rounded text-xs md:py-1 md:px-2 md:text-sm">Agregar Ingrediente</button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-1 rounded text-xs md:py-1 md:px-2 md:text-sm"
+          onClick={handleOpenModal}
+        >
+          Agregar Ingrediente
+        </button>
       </div>
       <div className="card">
 
@@ -41,8 +58,8 @@ const InventoryPage: React.FC = () => {
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Fecha</th>
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider hidden md:table-cell">IDP</th>
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Nombre</th>
-                <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider hidden md:table-cell">Costo</th>
-                <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider hidden md:table-cell">CU</th>
+                <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider hidden md:table-cell">Costo Pedido</th>
+                <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider hidden md:table-cell">Costo Unitario</th>
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Precio</th>
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Stock Inicial</th>
                 <th scope="col" className="px-1 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Stock Actual</th>
