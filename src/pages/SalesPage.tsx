883d613c3d5404fa95a873e61a29b16254769939
KvS-08 <kvsolutionshn@gmail.com>
2025-06-16 12:55:00 -0600

    Updated SalesPage.tsx

---
+++ src/pages/SalesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SalesPage = () => {
+  return (
+    <div>
+      <h1>Sales Page</h1>
+      <p>Welcome to the Sales Page!</p>
+    </div>
+  );
+};
+
+export default SalesPage;
